860ec3ed29e2dd4d187651177913400e6d17d9c1
Francis <francis13322@gmail.com>
2026-04-29 11:07:25 -0300

    CO4 - Manipulando o DOM

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,21 +1,32 @@
 class CalcController {
   constructor() {
     this._displayCalc = "0";
-    this._dataAtual;
+    this._currentDate;
+    this.initialize();
+  }
+
+  initialize() {
+    let displayCalcEl = document.querySelector("#display");
+    let datecEl = document.querySelector("#data");
+    let timeEl = document.querySelector("#hora");
+
+    displayCalcEl.innerHTML = "4567"
+    datecEl.innerHTML = "29/04/2026"
+    timeEl.innerHTML = "11:10"
   }
 
   get displayCalc() {
-    return this._displayCalc;
+    return this.displayCalc;
   }
   set displayCalc(valor) {
     this.displayCalc = valor;
   }
 
   get dataAtual() {
-    return this.dataAtual;
+    return this.currentDate;
   }
 
-  set dataAtual(valor) {
-    this.dataAtual = valor;
+  set currentDate(valor) {
+    this.currentDate = valor;
   }
 }
