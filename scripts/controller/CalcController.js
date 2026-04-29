11207f1d08f8b1807c8690a392b8202b250a8f40
Francis <francis13322@gmail.com>
2026-04-29 17:51:22 -0300

    CO9 - Adicionado Switch

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,5 +1,6 @@
 class CalcController {
   constructor() {
+    this._operation = [];
     this._locale = "pt-BR";
     this._displayCalcEl = document.querySelector("#display");
     this._datecEl = document.querySelector("#data");
@@ -15,23 +16,75 @@ class CalcController {
     }, 1000);
   }
 
-addEventListenerAll(element, events, fn){
+  addEventListenerAll(element, events, fn) {
+    events.split(" ").forEach((event) => {
+      element.addEventListener(event, fn, false);
+    });
+  }
+  clearAll() {
+    this._operation = []
+  }
 
-  events.split(" ").forEach(event =>{
-    element.addEventListener(event, fn, false)
-  })
+  clearEntry() {
+    this._operation.pop()
+  }
 
-}
+  setError(){
+    this.displayCalc = "Error"
+  }
+
+  addOperation(value){
+    this._operation.push(value)
+    console.log(this._operation)
+  }
+  execBtn(value) {
+      switch (value) {
+        case "ac":
+          this.clearAll();
+          break;
+        case "ce":
+          this.clearEntry();
+          break;
+        case "soma":
+          break;
+        case "subtracao":
+          break;
+        case "divisão":
+          break;
+        case "multiplicacao":
+          break;
+        case "porcento":
+          break;
+        case "igual":
+          break;
+        case "0":
+        case "1":
+        case "2":
+        case "3":
+        case "4":
+        case "5":
+        case "6":
+        case "7":
+        case "8":
+        case "9":
+            this.addOperation(parseInt(value))
+            break;
+        default:
+          this.setError();
+          break;
+      }
+    }
 
   initButtonsEvents() {
     let buttons = document.querySelectorAll("#buttons > g, #parts > g");
     buttons.forEach((btn, index) => {
-      this.addEventListenerAll(btn, "click drag", e => {
-        console.log(btn.className.baseVal.replace("btn-", ""));
+      this.addEventListenerAll(btn, "click drag", (e) => {
+        let textBtn = btn.className.baseVal.replace("btn-", "");
+        this.execBtn(textBtn);
       });
-      this.addEventListenerAll(btn, "mouseover mouseup mousedown", e =>{
-        btn.style.cursor = "pointer"
-      })
+      this.addEventListenerAll(btn, "mouseover mouseup mousedown", (e) => {
+        btn.style.cursor = "pointer";
+      });
     });
   }
 
