08e5cd85053f605a59c3fe5d389e6442566eeb13
Francis <francis13322@gmail.com>
2026-05-01 20:06:26 -0300

    C21 - Tratando erro com Try Catch

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -140,8 +140,14 @@ class CalcController {
   }
 
   getResult(){
+    try{
+      return eval(this._operation.join(""))
 
-    return eval(this._operation.join(""))
+    }catch(e){
+      setTimeout(()=>{
+        this.setError()
+      }, 1)
+    }
   }
   calc(){
     let last = ""
