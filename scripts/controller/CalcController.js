fec1ac42470f153f04b9176163cc13a75b4ec413
Francis <francis13322@gmail.com>
2026-05-01 19:55:26 -0300

    C20 - Limitando quantidade de numeros

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -307,6 +307,10 @@ class CalcController {
   }
 
   set displayCalc(value) {
+    if(value.toString().length > 10){
+      this.setError()
+      return false
+    }
     this._displayCalcEl.innerHTML = value;
   }
 
